6800861e7898582d3059773e16fd3cf2de29c814
Dalton Rocha da Silva <rochadalton00@gmail.com>
2026-07-24 13:50:36 -0300

    chore: configura frontend para usar API em producao

--- lavafast-web/src/api/axios.js
+++ lavafast-web/src/api/axios.js
@@ -1,13 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-
-    baseURL: 'http://localhost:3000/api',
-
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
-
         'Content-Type': 'application/json'
-
     }
-
 });
